a2371aaea2d1ac884e181fa7276ae52bcd4d4743
carlosmorais01 <carlos.morais@discente.ufg.br>
2024-04-13 00:45:00 -0300

    adicionando questao 2 e corrigindo 1

--- lista01-go/questao1/questao1.go
+++ lista01-go/questao1/questao1.go
@@ -10,9 +10,14 @@ func main() {
 	fmt.Println("Escreva as três notas")
 	fmt.Scan(&nota1, &nota2, &nota3)
 	if nota1 < 0 || nota2 < 0 || nota3 < 0 {
-		fmt.Print("Nota digitada inválida\n")
+		fmt.Println("Nota digitada inválida")
 		return
 	}
 	m = ((nota1 + nota2 + nota3) / 3)
 	fmt.Printf("A média do aluno é %.2f\n", m)
+	if m < 6 {
+		fmt.Print("REPROVADO\n")
+	} else {
+		fmt.Print("APROVADO\n")
+	}
 }
--- lista01-go/questao2/questao2.go
+++ lista01-go/questao2/questao2.go
@@ -12,8 +12,8 @@ func main() {
 
 	for n := 0; n < int(nJogos); n++ {
 		fmt.Println("Digite a quantidade de ingressos e as porcentagens equivalentes a cada categoria")
-		fmt.Scan(&ingQ, &pP, &pG, &pA, &pG)
-		valorTotal = (ingQ * (pP / 100) * 1) + (ingQ * (pG / 100) * 5) + (ingQ * (pA / 100) * 10) + (ingQ * (pC / 100) * 20)
-		fmt.Printf("A RENDA DO JOGO %v É = %v\n", n, valorTotal)
+		fmt.Scan(&ingQ, &pP, &pG, &pA, &pC)
+		valorTotal = ingQ * (pP/100*1 + pG/100*5 + pA/100*10 + pC/100*20)
+		fmt.Printf("A RENDA DO JOGO %v É = %.2f\n", n+1, valorTotal)
 	}
 }
