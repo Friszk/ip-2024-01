b84d5d101e0759cd62fcff4167480cf055e17fbc
Carlos Henrique Alves Morais <166178484+carlosmorais01@users.noreply.github.com>
2024-04-13 02:37:37 -0300

    Update questao3.go

--- lista01-go/questao3/questao3.go
+++ lista01-go/questao3/questao3.go
@@ -10,7 +10,7 @@ var n1, n2, n3 int
 func main() {
 	fmt.Println("Digite os 3 números")
 	fmt.Scan(&n1, &n2, &n3)
-	if n1 > 0 && n1 < 10 || n2 > 0 && n2 < 10 || n3 > 0 && n3 < 10 {
+	if n1 > 0 && n1 < 10 || n2 < 10 || n3 < 10 {
 		s := fmt.Sprintf("%v%v%v", n1, n2, n3)
 		n, _ := strconv.Atoi(s)
 		fmt.Println(n, n*n)
