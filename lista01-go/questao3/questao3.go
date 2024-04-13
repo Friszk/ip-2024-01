23e3c526e528be00b8ab79c4596bbe3412dc6e73
Carlos Henrique Alves Morais <166178484+carlosmorais01@users.noreply.github.com>
2024-04-13 02:41:30 -0300

    Update questao3.go

--- lista01-go/questao3/questao3.go
+++ lista01-go/questao3/questao3.go
@@ -4,18 +4,17 @@ import (
 	"fmt"
 	"strconv"
 )
-
 var n1, n2, n3 int
 
 func main() {
 	fmt.Println("Digite os 3 números")
 	fmt.Scan(&n1, &n2, &n3)
-	if n1 > 0 && n1 < 10 || n2 < 10 || n3 < 10 {
+	if n1 > 0 && n1 < 10 && n2 < 10 && n3 < 10 {
 		s := fmt.Sprintf("%v%v%v", n1, n2, n3)
 		n, _ := strconv.Atoi(s)
 		fmt.Println(n, n*n)
 	} else {
-	fmt.Println("O primeiro dígito não pode ser 0")
+	fmt.Println("DÍGITO INVÁLIDO")
 	return
 }
 }
