aade4cfa19781c6c29c0fba3eb5ee0aefedc05bd
Carlos Henrique Alves Morais <166178484+carlosmorais01@users.noreply.github.com>
2024-04-13 02:36:35 -0300

    Update questao3.go
    corrigindo erros

--- lista01-go/questao3/questao3.go
+++ lista01-go/questao3/questao3.go
@@ -10,11 +10,12 @@ var n1, n2, n3 int
 func main() {
 	fmt.Println("Digite os 3 números")
 	fmt.Scan(&n1, &n2, &n3)
-	if n1 == 0 {
-		fmt.Println("O primeiro dígito não pode ser 0")
-		return
-	}
-	s := fmt.Sprintf("%v%v%v", n1, n2, n3)
-	n, _ := strconv.Atoi(s)
-	fmt.Println(n, n*n)
+	if n1 > 0 && n1 < 10 || n2 > 0 && n2 < 10 || n3 > 0 && n3 < 10 {
+		s := fmt.Sprintf("%v%v%v", n1, n2, n3)
+		n, _ := strconv.Atoi(s)
+		fmt.Println(n, n*n)
+	} else {
+	fmt.Println("O primeiro dígito não pode ser 0")
+	return
+}
 }
