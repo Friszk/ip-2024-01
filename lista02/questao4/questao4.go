28361928ea3961b5f92de57a4db2a631dc89f424
carlosmorais01 <carlos.morais@discente.ufg.br>
2024-04-17 13:07:36 -0300

    otimizações e correções de bugs

--- lista02/questao4/questao4.go
+++ lista02/questao4/questao4.go
@@ -12,38 +12,34 @@ func main() {
 	fmt.Scan(&n, &i, &K, &s)
 
 	fmt.Println("Tabuada de soma")
-	r = n + i
-	fmt.Printf("%.2f + %.2f = %.2f\n", n, i, r)
 
-	for o := 0.0; o < i; o++ {
-		r = n + (i + s)
-		fmt.Printf("%.2f + %.2f = %.2f\n", n, i+s, r)
+	for o, in := 0.0, 0.0; o < K; o++ {
+		r = n + (i + in)
+		in += s
+		fmt.Printf("%.2f + %.2f = %.2f\n", n, in, r)
 	}
 
 	fmt.Println("Tabuada de subtração")
-	r = n - i
-	fmt.Printf("%.2f - %.2f = %.2f\n", n, i, r)
 
-	for o := 0.0; o < i; o++ {
-		r = n - (i + s)
-		fmt.Printf("%.2f - %.2f = %.2f\n", n, i+s, r)
+	for o, in := 0.0, 0.0; o < K; o++ {
+		r = n - (i + in)
+		in += s
+		fmt.Printf("%.2f - %.2f = %.2f\n", n, in, r)
 	}
 
 	fmt.Println("Tabuada de multiplicação")
-	r = n * i
-	fmt.Printf("%.2f x %.2f = %.2f\n", n, i+s, r)
 
-	for o := 0.0; o < i; o++ {
-		r = n * (i + s)
-		fmt.Printf("%.2f x %.2f = %.2f\n", n, i, r)
+	for o, in := 0.0, 0.0; o < K; o++ {
+		r = n * (i + in)
+		in += s
+		fmt.Printf("%.2f x %.2f = %.2f\n", n, in, r)
 	}
 
 	fmt.Println("Tabuada de divisão")
-	r = n / i
-	fmt.Printf("%.2f / %.2f = %.2f\n", n, i+s, r)
 
-	for o := 0.0; o < i; o++ {
-		r = n / (i + s)
-		fmt.Printf("%.2f / %.2f = %.2f\n", n, i, r)
+	for o, in := 0.0, 0.0; o < K; o++ {
+		r = n / (i + in)
+		in += s
+		fmt.Printf("%.2f / %.2f = %.2f\n", n, in, r)
 	}
 }
