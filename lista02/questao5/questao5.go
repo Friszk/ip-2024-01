aba68476d3e0fda57ab566c87bb61b213fe6f7f3
carlosmorais01 <carlos.morais@discente.ufg.br>
2024-04-18 00:39:59 -0300

    adicionando questão 5

--- lista02/questao5/questao5.go
+++ lista02/questao5/questao5.go
@@ -4,6 +4,41 @@ import (
 	"fmt"
 )
 
+var tamanhoLista int
+
 func main() {
+
+	fmt.Println("Digite o comprimento da lista e em seguida a lista dos números")
+	fmt.Scan(&tamanhoLista)
+
+	Lista := make([]int, tamanhoLista)
+
+	for i := 0; i < tamanhoLista; i++ {
+		fmt.Scan(&Lista[i])
+	}
+
+	maiorCompEncontrado := make([]int, 0, 1)
+	maiorCompAtual := []int{Lista[0]}
+
+	for i := 1; i < tamanhoLista; i++ {
+		if Lista[i] > maiorCompAtual[len(maiorCompAtual)-1] {
+
+			maiorCompAtual = append(maiorCompAtual, Lista[i])
+
+		} else {
+			maiorCompAtual = []int{Lista[i]}
+		}
+
+		if len(maiorCompAtual) > len(maiorCompEncontrado) {
+			maiorCompEncontrado = maiorCompAtual
+		}
+
+	}
+
+	if maiorCompEncontrado[0] == 0 {
+		fmt.Println("O comprimento do segmento crescente máximo é: 0")
+	} else {
+		fmt.Printf("O comprimento do segmento crescente máximo é: %v\n", len(maiorCompEncontrado)-1)
+	}
 
 }
