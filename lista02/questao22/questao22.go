a0fdeb68accda52ffb7e9813cac7c37c8ab625fc
Carlos Henrique Alves Morais <166178484+carlosmorais01@users.noreply.github.com>
2024-04-26 13:52:04 -0300

    Update questao22.go

--- lista02/questao22/questao22.go
+++ lista02/questao22/questao22.go
@@ -4,6 +4,7 @@ import "fmt"
 func main() {
     
     var n float64
+    fmt.Println("Digite o número real para descobrir sua fração originária")
     fmt.Scan(&n)
     
     for i:= 1.0; i > 0; i++ {
