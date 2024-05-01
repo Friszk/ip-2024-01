38950bce14ac3517b35bd6dfb1333f004dc8ed3e
Aluno <carlos.morais@discente.ufg.br>
2024-04-30 21:43:59 -0300

    adicionando questão 2

--- lista03/questão2/questão2.go
+++ lista03/questão2/questão2.go
@@ -3,5 +3,30 @@ package main
 import "fmt"
 
 func main() {
-	
+	var n, maior, maiores int
+
+	for {
+		fmt.Scan(&n)
+		if n >= 1 && n <= 1000 {
+			break
+		} else {
+			fmt.Println("O número precisa ser > 1 e < 1000")
+		}
+	}
+
+	numeros := make([]int, n)
+
+	for i := 0; i < n; i++ {
+		fmt.Scan(&numeros[i])
+	}
+
+	fmt.Scan(&maior)
+
+	for i := 0; i < n; i++ {
+		if numeros[i] >= maior {
+			maiores++
+		}
+	}
+
+	fmt.Println(maiores)
 }
