1e61e580109ed663d40b801150518d127412c493
Aluno <carlos.morais@discente.ufg.br>
2024-05-13 22:07:59 -0300

    questão 25

--- lista03/questão-25/questão-25.go
+++ lista03/questão-25/questão-25.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
+	var acertos int
+	sorteado := make([]int, 6)
+	for i := range sorteado {
+		fmt.Scan(&sorteado[i])
+	}
+
+	n := 0
+	fmt.Scan(&n)
+	for i := 0; i < n; i++ {
+		aposta := make([]int, 6)
+		for i := range aposta {
+			fmt.Scan(&aposta[i])
+		}
+		for i := range aposta {
+			for j := range sorteado {
+				if aposta[i] == sorteado[j] {
+					acertos++
+					break
+				}
+			}
+		}
+		fmt.Println(acertos)
+		acertos = 0
+	}
 
 }
